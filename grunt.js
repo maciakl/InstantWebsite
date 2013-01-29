5896c437f8a4d9083eb081956b249b16324a3a58
Lukasz Grzegorz Maciak <maciakl@gmail.com>
2013-01-29 17:13:43 -0500

    First Commit

---
+++ grunt.js
@@ -0,0 +1,65 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    lint: {
+      files: ['grunt.js', 'lib/**/*.js', 'test/**/*.js']
+    },
+    qunit: {
+      files: ['test/**/*.html']
+    },
+    watch: {
+      files: ['index.html'],
+      tasks: 'lint qunit'
+    },
+    jshint: {
+      options: {
+        curly:  true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        boss: true,
+        eqnull: true,
+        browser: true
+      },
+      globals: {
+        $: false,
+        jQuery: false,
+        // Underscore.js
+        _: false,
+        // Gooogle Maps Globals
+        google: false,
+        // Qunit Assertions
+        assert: false,
+        asyncTest: false,
+        deepEqual: false,
+        equal: false,
+        expect: false,
+        module: false,
+        notDeepEqual: false,
+        notEqual: false,
+        notStrictEqual: false,
+        ok: false,
+        QUnit: false,
+        raises: false,
+        start: false,
+        stop: false,
+        strictEqual: false,
+        test: false
+      }
+    },
+    server: {
+        port: 3000,
+        base: '.'
+    }
+  });
+
+  // Default task.
+  grunt.registerTask('default', 'lint server watch');
+
+};
---
+++ readme.markdown
@@ -0,0 +1,41 @@
+Instant Website
+===
+
+Scaffold a very simple, single page website using Twitter Bootstrap and jQuery in mere seconds.
+
+Prerequisites
+---
+
+You need to have the following installed on your computer:
+
+* [node.js][n]
+* [bower][b]
+* [grunt.js][g]
+
+Bootstrap The New Website
+---
+
+To start a new website, clone this repository, then do:
+
+    bower install
+    grunt
+
+This will download all the required files (Twitter Bootrstrap assets and jQuery scripts) and start a webserver. Now you can just point your browser at `http://localhost:3000` and see the page as if it was served from the web. That's about it.
+
+You can add custom styles in `lib/custom.css` and custom scripts in `lib/custom.js`.
+
+Grunt Tasks
+---
+
+Following grunt tasks are defined:
+
+* **lint** - will run *JSHint* on all scripts in `lib` and `test` directories
+* **qunit** - will attempt to run Qunit unit tests from `test` directory
+* **watch** - continuously lint files as you edit
+* **server watch** - start a web server on port 3000
+
+The default task is `lint server watch`.
+
+[n]: http://nodejs.org/
+[b]: http://twitter.github.com/bower/
+[g]: http://gruntjs.com/
